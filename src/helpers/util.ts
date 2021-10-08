6a870db64363b63409b8acc920a26dced05b6e09
甜瓜 <tiangua_6657970@163.com>
2021-10-08 19:32:16 +0800

    feat(test helpers files): 单元测试
    对helper模块进行单元测试

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -9,7 +9,7 @@ export function isDate(val: any): val is Date {
 // }
 
 export function isPlainObject(val: any): val is Object {
-  return toString.call(val) === '[object object]'
+  return toString.call(val) === '[object Object]'
 }
 
 export function isFormData(val: any): val is FormData {
